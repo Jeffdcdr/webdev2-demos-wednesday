5c5d54194b26f3a22119f20c871498eeb373d5b6
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-09-29 23:52:04 -0600

    added week-4a
    4a as the activity with and added the reset button for practice

---
+++ app/week-4a/page.js
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Page() {
+  const [count, setCount] = useState(0);
+
+  // TODO: Create a new state variable called 'isEnabled' with an initial value of false.
+  const [isEnabled, setIsEnabled] = useState(false);
+  // This variable will be used to control the enabled/disabled state of the increment button.
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const toggleEnabled = () => {
+    // TODO: Implement a function that toggles the value of 'isEnabled'.
+    setIsEnabled(!isEnabled);
+    // If 'isEnabled' is currently true, it should be set to false, and vice versa.
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={increment}
+        disabled={!isEnabled}
+        className="bg-blue-400 disabled:bg-yellow-900 rounded m-4 p-4"
+      >
+        +1
+      </button>
+      <input type="checkbox" onChange={toggleEnabled} />
+      Enable button
+      <p>Current count: {count}</p>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
