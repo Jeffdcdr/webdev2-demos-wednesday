d29f2a6ba346f9c47ae45fd910f39924ca7220b7
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-02 12:10:22 -0600

    Update dog-form.js
    testing input box

--- app/week-5/dog-form.js
+++ app/week-5/dog-form.js
@@ -7,14 +7,21 @@ export default function DogForm() {
     const [breed, setBreed] = useState("");
     const [age, setAge] = useState(0);
 
-
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
 
     return(
         <div className="m-2">
             <h2 className="text-2xl">Add a dog</h2>
             <form className="m-24">
                 <label htmlFor="name"></label>
-
+                <input
+                id="name"
+                type="text"
+                value={name}
+                onChange={(event) => handleNameChange = (event)}
+                />
             </form>
         </div>
 
