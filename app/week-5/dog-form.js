1debd8993ca14505342be6dc5b1dbe84217ea25f
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-03 14:44:59 -0600

    Update dog-form.js

--- app/week-5/dog-form.js
+++ app/week-5/dog-form.js
@@ -4,8 +4,8 @@ import { useState } from "react";
 
 export default function DogForm() {
     const [name, setName] = useState("");
-    const [breed, setBreed] = useState("");
-    const [age, setAge] = useState(0);
+    // const [breed, setBreed] = useState("");
+    // const [age, setAge] = useState(0);
 
     const handleNameChange = (event) => {
         setName(event.target.value);
@@ -15,12 +15,12 @@ export default function DogForm() {
         <div className="m-2">
             <h2 className="text-2xl">Add a dog</h2>
             <form className="m-24">
-                <label htmlFor="name"></label>
+                <label htmlFor="name"> Name: </label>
                 <input
                 id="name"
                 type="text"
                 value={name}
-                onChange={(event) => handleNameChange = (event)}
+                onChange={(event) => handleNameChange (event)}
                 />
             </form>
         </div>
