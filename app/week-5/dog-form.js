769bd7ba5b66c8aef743c2f8681623148104d5d9
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-07 00:09:10 -0600

    Update dog-form.js
    Adding the components handling the functions differently, name has a different approach, breed and age has a simpler one but both works just fine

--- app/week-5/dog-form.js
+++ app/week-5/dog-form.js
@@ -4,25 +4,52 @@ import { useState } from "react";
 
 export default function DogForm() {
     const [name, setName] = useState("");
-    // const [breed, setBreed] = useState("");
-    // const [age, setAge] = useState(0);
+    const [breed, setBreed] = useState("");
+    const [age, setAge] = useState(0);
 
     const handleNameChange = (event) => {
         setName(event.target.value);
-    }
+    };
+
+
 
     return (
+      //Name Handler
       <div className="m-2">
         <h2 className="text-2xl">Add a dog</h2>
-        <form className="m-24">
+        <form className="m-2">
           <label htmlFor="name"> Name: </label>
           <input
             id="name"
             type="text"
             value={name}
             onChange={(event) => handleNameChange(event)}
+            className="border border-gray-400 m-2"
           />
-        </form>
+          {/* Breed: <input type="text" name="breed"></input> */}
+          <label htmlFor="breed"> Breed: </label>
+          <input
+            id="breed"
+            type="text"
+            value={breed}
+            onChange={(event) => setBreed(event.target.value)} //Different way of handling the event
+            className="border border-gray-400 m-2"
+          />
+            <label htmlFor="age"> Age: </label>
+            <input
+                id="age"
+                type="number"
+                value={age}
+                onChange={(event) => setAge(event.target.value)}
+                className="border border-gray-400 m-2"
+            />
+        </form>   
+        {/* OUTPUT HANDLERS     */}
+        <div className="text-lg"> 
+            <p>Name: {name}</p>
+            <p>Breed: {breed}</p>
+            <p>Age: {age}</p>
+        </div>
       </div>
     );
 }
