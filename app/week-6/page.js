b3f1577040caf9be003cb53bff750566bfbd89e6
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-09 12:15:54 -0600

    Created public folder under app

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,11 +1,20 @@
-import DogForm from "./dog-form";
+
 
 
 export default function Page() {
+    
+    let dogs = [
+        { name: "Shnukems", description: "Adorable", imageUrl: "/images/dog1.jpg" },
+        { name: "Barky", description: "Loud", imageUrl: "/images/dog2.jpg" },
+        { name: "Puddles", description: "Wet", imageUrl: "/images/dog3.jpg" },
+        
+    ];
+
+
     return (
         <main className="p-2">
             <h1 className="text-2xl font-semibold">Week 6 interactivity with forms</h1>            
-            <DogForm/>
+            
         </main>
     );
 }
