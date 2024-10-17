35734557c114127d9eb4261644e9ac2987d0e21c
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-17 02:20:48 -0600

    Added Week 7 up to 13 page place holders
    Added Week 7 up to 13 page place holders

---
+++ app/week-12/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="p-2">
+      <h1 className="text-2xl font-semibold"> Week 12
+         Demo </h1>
+      <div></div>
+    </main>
+  );
+}
---
+++ app/week-7/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="p-2">
+      <h1 className="text-2xl font-semibold"> Week 7 Demo </h1>
+      <div></div>
+    </main>
+  );
+}
