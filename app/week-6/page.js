79b91947e2601e17b27f20b0cf23f679e9484f76
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-17 23:36:21 -0600

    Update page.js
    adding use state, use client, import state
    adding constant for handleclick on a particular id to setselectedDogId

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,18 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
 import Image from "next/image";
 import dogsJson from "./dogs.json"; //dogsJson is an array of dog objects
 
 export default function Page() {
+  const [selectedDogId, setSelectedDogId] = useState(-1); // selectedDogId is a state variable
   let dogs = [...dogsJson]; // shallow copy of dogsJson #1
 
-  // sorting dogs by name Option #1
+  // SORTING dogs by name Option #1
   dogs.sort((a, b) => a.name.localeCompare(b.name)); // sort by name > localeCompare is built-in JS method
 
-  // sorting dogs by name Option #2
+  // SORTING dogs by name Option #2
   // dogs.sort((a, b) => {
   //   if (a.name < b.name) return -1;
   //   if (a.name < b.name) return 1;
   //   return 0;
   // });
+
+  // FILTERING dogs
+  dogs = dogs.filter((dog) => dog.name.length > 5); // filter dogs with name length > 5
+  
+  const handleClick = (id) => {
+    setSelectedDogId(id);
+  };
 
   return (
     <main className="p-2">
@@ -20,7 +32,7 @@ export default function Page() {
       <ul>
         {/* {dogsJson.map((dog) => (   #1 */}
         {dogs.map((dog) => (
-          <li key={dog.name} className="py-2">
+          <li key={dog.name} className="py-2 bg-slate-400 m-2" >
             <h2 className="text-lg font-semibold">{dog.name}</h2>
             <p className="text-sm">{dog.description}</p>
             <p>
