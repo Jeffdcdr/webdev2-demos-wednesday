e6387850b1d0bf758e02d89956c697c7f94a6ace
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-17 23:14:16 -0600

    Update page.js
    updating week6 demo

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,31 +1,26 @@
 import Image from "next/image";
-import dogsJson from "./dogs.json"; 
+import dogsJson from "./dogs.json"; //dogsJson is an array of dog objects
 
 export default function Page() {
+    let dogs = [...dogsJson]; // shallow copy of dogsJson
     
-    // let dogs = [
-    //     { name: "Shnukems", description: "Adorable", imageUrl: "/images/dog1.jpg" },
-    //     { name: "Barky", description: "Loud", imageUrl: "/images/dog2.jpg" },
-    //     { name: "Puddles", description: "Wet", imageUrl: "/images/dog3.jpg" },
-        
-    // ];
-
 
     return (
       <main className="p-2">
-        <h1 className="text-2xl font-semibold">
-          Week 6 Demo
-        </h1>
+        <h1 className="text-2xl font-semibold">Week 6 Demo</h1>
         <ul>
-          {dogsJson.map((dog) => (
-            <li key={dog.name}>
+          {dogs.map((dog) => (
+            <li key={dog.name} className="py-2">
               <h2 className="text-lg font-semibold">{dog.name}</h2>
               <p className="text-sm">{dog.description}</p>
-              <Image 
-                src={dog.imageUrl} 
-                alt={dog.name}
-                width={640}
-                height={480} />
+              <p>
+                <Image
+                  src={dog.imageUrl}
+                  alt={dog.name}
+                  width={640}
+                  height={480}
+                />
+              </p>
             </li>
           ))}
         </ul>
