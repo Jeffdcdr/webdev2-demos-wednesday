72e376ba8334923a48323797b671697dde91196a
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-29 22:56:02 -0600

    Update page.js
    updating page

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 // import Counter from "./counter";
 import DogList from "./dog-list";
 import DogForm from "./dog-form";
@@ -8,13 +10,21 @@ import { useState } from "react"; //importing useState from react
 export default function Page() {
   const [dogs, setDogs] = useState(dogData); 
 
+  const handleAddDog = (newDog) => {
+    setDogs([...dogs, newDog]);
+  };
+
   return (
     <main className="p-2">
       <h1 className="text-2xl font-semibold"> Week 7 Demo </h1>
       <div></div>
       {/* <Counter /> */}
-      <DogList />
-      <DogForm  />
+      {/* <DogList /> */}
+      <DogList dogs={dogs} />
+      
+      {/* onAddDog is a custom prop that will be passed to DogForm */}
+      <DogForm onAddDog={handleAddDog}/> 
+
     </main>
   );
 }
