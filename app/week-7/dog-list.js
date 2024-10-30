af7f71620c3cd18b58ad0a41e3bf6eeec914e1c0
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-29 22:48:28 -0600

    Update dog-list.js
    dogData.map is gone because it is being called in the page, changed dogData to dogs and made as a prop

--- app/week-7/dog-list.js
+++ app/week-7/dog-list.js
@@ -1,12 +1,14 @@
-
+// import dogData from "./dog-data.json";
 import Dog from "./dog.js";
 
-export default function DogList() {
+// export default function DogList() {
+export default function DogList({dogs}) {
   return (
     <div>
       <h2>Dog List</h2>
 
-      {dogData.map((dog) => (
+      {/* {dogData.map((dog) => ( */}
+      {dogs.map((dog) => (
         <Dog key={dog.id} name={dog.name} age={dog.age} />
       ))}
     </div>
