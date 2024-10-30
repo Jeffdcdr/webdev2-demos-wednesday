b898fec8db42adc745bad981927bcaeb96c6d546
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-29 22:07:16 -0600

    Update dog-list.js
    Formatting update

--- app/week-7/dog-list.js
+++ app/week-7/dog-list.js
@@ -1,17 +1,14 @@
-import dogData from './dog-data.json';
-import Dog from './dog.js';
+import dogData from "./dog-data.json";
+import Dog from "./dog.js";
 
 export default function DogList() {
-    return (
-        <div>
-            <h2>Dog List</h2>
-            
-            {dogData.map((dog) => (
-                
-                <Dog key={dog.id} name={dog.name} age={dog.age} />
-                
-            ))}
-            
-        </div>
-    );
+  return (
+    <div>
+      <h2>Dog List</h2>
+
+      {dogData.map((dog) => (
+        <Dog key={dog.id} name={dog.name} age={dog.age} />
+      ))}
+    </div>
+  );
 }
